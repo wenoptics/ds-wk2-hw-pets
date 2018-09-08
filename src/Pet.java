87c44dfce6ea08f21ec938d8986ecf210c79a3df
Grayson Wen <wenoptics@163.com>
2018-09-08 14:30:36 -0400

    init commit; the generic Pet class

---
+++ src/Pet.java
@@ -0,0 +1,58 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ *
+ */
+public abstract class Pet {
+
+    public enum Species {
+        DOG, CAT, BIRD, OTHER
+    }
+
+    /**
+     * The name of the Pet
+     */
+    private String name;
+
+    /**
+     * Species of the Pet
+     *      Species is read-only (which has not setter)
+     */
+    private Species species;
+
+    /**
+     * Returns the name of the pet
+     * @return the name string
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the name of the pet
+     * @param name   a new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the species of the pet
+     * @return      a species enum
+     */
+    public Species getSpecies() {
+        return species;
+    }
+
+    /**
+     * Returns the sound of the pet
+     * @return      a sound the pet would make
+     */
+    public String Say() {
+        throw new NotImplementedException();
+    }
+
+    public abstract float VetAppointment();
+
+
+}
---
+++ src/PetTrick.java
@@ -0,0 +1,3 @@
+public interface PetTrick {
+    public String Trick();
+}
